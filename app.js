2e83863f0f15adc97944ae67337cd83517b9a3e1
sajid hamza <sajid1999.sh@gmail.com>
2020-05-15 18:24:15 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+$('#responsive').lightSlider({
+        item:1,
+        loop:true,
+        slideMove:1,
+        pauseOnHover: true,
+        auto:true,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:600
+    });
+  });
